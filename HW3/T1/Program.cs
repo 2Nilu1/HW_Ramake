34df5f01868233c5597524b88d926256c0e86dee
Nilu <21nilu@mail.ru>
2022-12-06 17:24:09 +0300

    HW3

---
+++ HW3/T1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число
+//и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+if(num.Length == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine($"{num} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{num} - не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"Число:{num} - не является пятизначным,повторите попытку");
+}
---
+++ HW3/T2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек
+//и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+
+Console.WriteLine("Введите х первой точки ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите у первой точки ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите z первой точки ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите х второй точки ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите у второй точки ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите z второй точки ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double A = x2 - x1;
+double B = y2 - y1;
+double C = z1 - z2;
+
+double dist = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Расстояние между точками = {dist}");
+
+
+
