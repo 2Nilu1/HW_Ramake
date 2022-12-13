a10addc3cfb41aac696766a92d5dc30457dc2a17
Nilu <21nilu@mail.ru>
2022-12-13 17:59:58 +0300

    HW6

---
+++ HW6/t1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Pflfxf 41.Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+//Вводим сразу все числа в одну линию, не через бесконечное кол-во ReadLine.
+Console.Clear();
+Console.WriteLine("Введите числа через пробел");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); //подсмотрел
+int count = 0;
+int Mzero(int[] array)
+{
+    for(int i =0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }return count;
+}
+Console.WriteLine($"Кол-во положительных чиесл = {Mzero(array)}");
---
+++ HW6/t2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// Точки для уравнений пользователь вводит с клавиатуры, можно через несколько ReadLine
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// Выполнение обеих задач с использованием созданных вами функций.
+Console.Clear();
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = 0;
+double y = 0;
+double Fun(double x)
+{
+    x = -(b1 - b2) / (k1 - k2);
+    return x;
+}
+
+
+double Fun1(double y)
+{
+    y = k1 * x + b1;
+    return y;
+}
+
+// if (k1==k2)
+// {
+//     Console.WriteLine("Прямые не пересекаютсяю");
+//     else
+//     {
+//         if(k1==k2; b1 > b2 || b1 < b2)
+//         {
+
+//         }
+//     }
+// }
+
+
+
+
+Console.WriteLine($"Координаты точки пересечения: ({Fun(x)}; {Fun1(y)})" );
+
+
+// double Fun(double b1, double b2, double k1, double k2)
+// {
+//     double x = -(b1 - b2) / (k1 - k2);
+//     return x;
+// }
+
+// double Fun1(double k1, double x, double b1)
+// {
+//     double y = k1 * x + b1;
+//     return y;
+// }
+
